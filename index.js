0a517273c769c723f55ab7c7d2df3927542cb483
Lucca Santos <lucasluccad.r@outlook.com>
2022-12-24 12:55:25 -0300

    Thought and User modules created

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const PORT = 3000
 // Connection with mysql
 const connection = require("./db/connection")
 
+// Models 
+const User = require("./models/User")
+const Thought = require("./models/Thought")
+
 // Template engine
 app.engine("handlebars", exphbs.engine())
 app.set("view engine", "handlebars")
@@ -42,6 +46,7 @@ app.use("*", (req, res) => {
 
 // Sync settings and models with database
 connection
+  // .sync({ force: true }) // Forçar a recriar tabelas e relacionamentos
   .sync()
   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
   .catch(err => console.log(err))
